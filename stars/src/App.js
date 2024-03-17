018af6f0df6fd5dfeac8fe5281b58200d89c6e16
Aleksey Gordeyev <agordeyev94@gmail.com>
2024-03-17 13:21:07 +0500

    Finished with stars

--- stars/src/App.js
+++ stars/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { Stars } from './components/Stars/Stars';
 
 function App() {
+  const count = 2
+  if ([1, 2, 3, 4, 5].includes(count)){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stars count={count}/>
   );
+  }
 }
 
 export default App;
